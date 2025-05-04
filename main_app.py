329cdde12f1505e8fc685883c82144943df175fa
Ayush <nairayush45@gmail.com>
2025-05-04 15:10:45 +0530

    file upload

--- main_app.py
+++ main_app.py
@@ -1,43 +1,48 @@
 import numpy as np
 import streamlit as st
 import cv2
-from keras.models import load_model
+from tensorflow.keras.models import load_model
+from PIL import Image
 
-model = load_model('C:\Users\Aryush\Desktop\PlantDiseasePrediction\plant_disease.h5')
+# Load trained model
+model = load_model('plant_disease.h5')
 
-#Name of class
+# Name of classes
 CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
 
-#SETTING TITLE OF APP
-st.title('Plant Disease Prediction')
-st.markdown("Upload an image of the plant leaf")
+# Set title of app
+st.title('🌿 Plant Disease Prediction')
+st.markdown("Upload an image of the plant leaf (.jpg or .jpeg only)")
 
-#uploading the plant image
-plant_image = st.file_uploader("Choose an image...", type = jpg)
+# Upload image
+plant_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
 submit = st.button('Predict')
 
-#on predict button click
+# On Predict button click
 if submit:
-
     if plant_image is not None:
+        filename = plant_image.name.lower()
 
-        #Convert the file to an opencv image 
-        file_bytes = np.asarray(bytearray(plant_image.read()), dtype = np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
+        if filename.endswith(('.jpg', '.jpeg')):
+            # Convert to OpenCV image
+            file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
+            opencv_image = cv2.imdecode(file_bytes, 1)
 
+            # Show the image
+            st.image(opencv_image, channels="BGR", caption="Uploaded Leaf Image")
+            st.write(f"Image shape: {opencv_image.shape}")
 
-        #Displaying the image
-        st.image(opencv_image, "BGR")
-        st.write(opencv_image.shape)
+            # Resize and prepare for prediction
+            opencv_image = cv2.resize(opencv_image, (256, 256))
+            opencv_image = opencv_image.reshape(1, 256, 256, 3)
 
-        #Resizingthe image 
-        opencv_image = cv2.resize(opencv_image, (256,256))
+            # Make prediction
+            Y_pred = model.predict(opencv_image)
+            result = CLASS_NAMES[np.argmax(Y_pred)]
 
-        #convertimage to 4 Dimension
-        opencv_image.shape = (1,256,256,3)
-
-        #Make prediction
-
-        Y_pred = model.predict(opencv_image)
-        result = CLASS_NAMES[np.argmax(Y_pred)]
-        st.title(str("This is" +result.split('-')[0]+ "leaf with " + result.split('-')[1]))
+            # Show prediction
+            st.title(f"This is a {result.split('-')[0]} leaf with {result.split('-')[1]}")
+        else:
+            st.error("Invalid file type. Please upload a .jpg or .jpeg image.")
+    else:
+        st.warning("Please upload an image before clicking Predict.")
